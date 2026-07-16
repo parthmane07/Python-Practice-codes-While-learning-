5ef7397425e61385fd5613116e2ee15d491d8081
Parth Mane <parthmane333@gmail.com>
2026-07-16 11:50:39 +0530

    Added Multiplication Table program

---
+++ AcceptingMarksAndStoringInDictionary.py
@@ -0,0 +1,7 @@
+MarksOfSub = {}
+
+MarksOfSub.update({"phy":int(input("Enter phy marks:"))})
+MarksOfSub.update({"chem":int(input("Enter chem marks:"))})
+MarksOfSub.update({"bio":int(input("Enter bio marks:"))})
+
+print(MarksOfSub)
