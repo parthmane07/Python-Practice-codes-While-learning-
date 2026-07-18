cbadda34c5c26cd273e1e840b0a157ea0aeb98ca
Parth Mane <parthmane333@gmail.com>
2026-07-18 11:55:33 +0530

    added new method

--- MultiplicationTableOfNumN.py
+++ MultiplicationTableOfNumN.py
@@ -1,3 +1,4 @@
+#1st Method with While loop:
 n = int(input("Enter a number :"))
 
 i = 1
@@ -5,3 +6,15 @@ i = 1
 while i<=10 :
     print(n*i)
     i=i+1
+
+
+
+
+#2nd Method with For loop:
+n = int(input("Enter a number :"))
+
+for i in range(n, (n*10)+1, n):
+    print(i)
+
+for i in range(1, 11):
+    print(n*i)
