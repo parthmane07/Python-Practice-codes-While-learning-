f889b3a523b3432d889e42a0b4d43804154b6a59
Parth Mane <parthmane333@gmail.com>
2026-08-16 10:22:12 +0530

    Made Some Changes

--- ExOfClassObjMethodInit.py
+++ ExOfClassObjMethodInit.py
@@ -8,11 +8,10 @@ class Student:
     def welcome(self):
         print("Welcome", self.name)
         print("Your age is", self.age)
+        print("student of", Student.clg_name)
 
 s1 = Student("Tanu", 18)
 s1.welcome()
-print(s1.clg_name)
 
 s2 = Student("Parth", 19)
 s2.welcome()
-print(s2.clg_name)
